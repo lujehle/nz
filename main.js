2e66ba71ac31c5f86420754b4f1ba8111cdf0092
lujehle <lukas-jehle@web.de>
2025-03-27 15:36:06 +0100

    Kommentare eingefügt

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
+/*
+Skript für die Nueseelandreise
+*/
+
+//Karte initialisieren
 var map = L.map('map').setView([-41.270833, 173.284167], 11);
+//Hintergrundkarte definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+        //Marker zeichnen
         var marker = L.marker([-41.270833, 173.284167]).addTo(map);
+        //Popup definieren und öffnen
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
